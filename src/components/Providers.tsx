40c65c3e7c82ac434d5fdf30ffac79bf50717b88
Constructora Colombia Apps <constructoracolombia2020@gmail.com>
2026-02-09 15:33:36 -0500

    Remove PWA components causing build errors

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -2,18 +2,14 @@
 
 import { ToastProvider } from "@/components/Toast";
 import { ThemeProvider } from "@/components/ThemeProvider";
-import { ServiceWorkerRegistration } from "@/components/ServiceWorkerRegistration";
-import { PWAInstall } from "@/components/PWAInstall";
 import { GlobalSearch } from "@/components/GlobalSearch";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <ThemeProvider>
       <ToastProvider>
-        <ServiceWorkerRegistration />
         <GlobalSearch />
         {children}
-        <PWAInstall />
       </ToastProvider>
     </ThemeProvider>
   );
